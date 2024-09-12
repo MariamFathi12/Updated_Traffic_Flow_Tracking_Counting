44795fae5f599e6c1c2716e369f6977371fb4f89
mariamfathi369 <mariam.fathi369@gmail.com>
2024-09-12 22:30:44 +0300

    no message

--- vehicle-counting-yolov5/tracker.py
+++ vehicle-counting-yolov5/tracker.py
@@ -279,8 +279,7 @@ def detect(opt):
                         fps, w, h = 30, im0.shape[1], im0.shape[0]
 
                     save_path = str(Path(save_path).with_suffix(".mp4"))  # force *.mp4 suffix on results videos
-                    save_path='/content/saved_output.mp4'
-                    vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (1000,700))
+                    vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'XVID'), fps, (1000,700))
                 vid_writer.write(im0)
 
     # Print results
